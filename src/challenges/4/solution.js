44de16edde61863f3b68347d0d8460a74a0325fe
danilupion <danilupion@gmail.com>
2023-12-04 23:36:59 +0100

    Challenge 4

---
+++ src/challenges/4/solution.js
@@ -0,0 +1,26 @@
+// A regex that matches all characters between parentheses excluding the parentheses themselves
+
+function decode(message) {
+  const matches = Array.from(message.matchAll(/\([^()]*\)/g)).map((m) => ({
+    match: m[0],
+    replacement: m[0].slice(1, -1).split("").reverse().join(""),
+  }));
+
+  return matches.length
+    ? decode(
+        matches.reduce(
+          (acc, { match, replacement }) => acc.replace(match, replacement),
+          message,
+        ),
+      )
+    : message;
+}
+
+const a = decode("hola (odnum)");
+console.log(a); // hola mundo
+//
+const b = decode("(olleh) (dlrow)!");
+console.log(b); // hello world!
+//
+const c = decode("sa(u(cla)atn)s");
+console.log(c); // santaclaus
